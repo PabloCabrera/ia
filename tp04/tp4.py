d41bcc4f0622a0b11fa6457f505f2bbc6f1cb6b6
Pablo Cabrera (en netrunner) <binchmod777@gmail.com>
2017-06-18 01:07:16 -0300

    tp4 resuelve el problema, falta graficar

--- tp04/tp4.py
+++ tp04/tp4.py
@@ -100,10 +100,10 @@ def generar_info_atributo (datos_agrupados, atributo):
 
 def seleccionar_atributo_menor_entropia (datos_agrupados):
 	atributo_menor = None
-	entropia_menor = 1.0
+	entropia_menor = 1.1
 	for atributo in datos_agrupados:
 		entropia_calculada = calcular_entropia_total (datos_agrupados[atributo])
-		if (entropia_calculada <= entropia_menor):
+		if (entropia_calculada < entropia_menor):
 			entropia_menor = entropia_calculada
 			atributo_menor = atributo
 	return atributo_menor
@@ -113,27 +113,68 @@ def main ():
 	texto_documento = iterar_nivel (datos, cabeceras)
 	imprimir_documento(texto_documento)
 
-def iterar_nivel (datos, cabeceras, arbol=None, camino=None):
+def iterar_nivel (datos, cabeceras, camino=None):
+	txt = ""
 	atributos_arboles = cabeceras[:]
 	atributos_arboles.remove("OBJETIVO")
 	agrupados = agrupar_por_atributo (datos, atributos_arboles, "OBJETIVO")
-	txt = dibujar_tabla_datos (datos, cabeceras)
-	txt += "\section {Iteración 1}\n"
+	if camino is None:
+		txt += "\section {Iteración 1}\n"
+	else:
+		txt_camino = ", ".join (map(lambda v: "=".join(v), camino))
+		txt += "\section {%s}\n" % txt_camino
+	txt += dibujar_tabla_datos (datos, cabeceras)
 	for atributo in agrupados:
 		txt += generar_info_atributo (agrupados, atributo)
 	atributo_menor_entropia = seleccionar_atributo_menor_entropia (agrupados)
 	txt += "\subsection {Resultado de cálculo de entropía}\n"
 	txt += "El atributo con menor entropía es %s\\\\\n" % atributo_menor_entropia
-	#arbol = dividir_arbol (arbol, camino, atributo_menor_entropia, agrupados[atributo_menor_entropia])
+	txt += dividir_arbol_decision (datos, cabeceras, atributo_menor_entropia, camino)
 	#dibujar_arbol_decision (arbol)
+	return txt
+
+def dividir_arbol_decision (datos, cabeceras, atributo, camino):
+	txt = ""
+	valores_analizar = []
+	posibles_valores = listar_valores_atributo (datos, atributo)
+	for valor in posibles_valores:
+		elementos = filtrar_datos (datos, atributo, valor)
+		if (len (elementos) > 0) and valor_discrimina_datos (atributo, valor, datos):
+			objetivo = elementos[0]["OBJETIVO"]
+			txt += "$%s = %s \implies {resultado} = %s$\\\\\n" % (atributo, valor, objetivo)
+		else:
+			txt += "El valor %s del atributo %s no discrimina los datos. Se procederá a hacer el análisis del subarbol.\\\\\n" % (valor, atributo)
+			valores_analizar.append (valor)
+	for valor in valores_analizar:
+		txt += analizar_subarbol (datos, cabeceras, atributo, valor, camino)
+	return txt
+
+def analizar_subarbol (datos, cabeceras, atributo, valor, camino):
+	txt = ""
 	if camino is None:
 		camino = []
+	else:
+		camino = camino[:]
+	camino.append ([atributo, valor])
+	datos_recortados = filtrar_datos (datos, atributo, valor)
+	txt += iterar_nivel (datos_recortados, cabeceras, camino)
 	return txt
+	
+def valor_discrimina_datos (atributo, valor, datos):
+	filtrados = filtrar_datos (datos, atributo, valor)
+	discrimina = False
+	if (len (filtrados) > 0) :
+		primer_objetivo = filtrados[0]["OBJETIVO"]
+		discrimina = True
+		for elemento in filtrados:
+			discrimina = (discrimina and (elemento["OBJETIVO"] == primer_objetivo))
+	return discrimina
 
 def imprimir_documento (texto_documento):
 	print """
 \documentclass{article}
 \usepackage[utf8]{inputenc}
+\usepackage{amsmath}
 \usepackage[table]{xcolor}
 \usepackage{tikz-qtree}
 \\begin {document}
